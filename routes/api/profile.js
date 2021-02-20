d3e0f1c9fa0fee9904fb6a84f35b5ecbeec5015d
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-02-20 22:30:12 +0800

    Gitignored

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -12,7 +12,7 @@ const { check, validationResult} = require('express-validator')
 router.get('/me', auth, async (req, res)=>{
     try{
         const profile = await Profile.findOne({user: req.user.id}).populate('user', ['name', 'avatar']);
-
+        
         if(!profile){
             res.status(400).json({msg: 'There is no profile for this user'});
         }
