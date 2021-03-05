c150241d94e1938d37e50d751e7923a3e8076c81
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-03-05 21:08:30 +0800

    added route to get profile by user_id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -108,4 +108,20 @@ router.post('/', [  auth, [
         
 })
 
+//@route    POST api/profile/user/:user_id
+//@desc     Get profile by user ID
+//@access   Public
+
+router.get('/user/:user_id', async(req, res) =>  {
+    try {
+        const profile = await Profile.findOne({user: req.params.user_id}).populate('user', ['name', 'avatar'])
+        if(!profile) 
+            return res.status(400).json({msg: 'There is no profile for this user'});
+        res.json(profile);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
 module.exports = router;
