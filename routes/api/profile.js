0ff73c8be2a8b10d9251fe20d1e3922a9cdc8d32
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-03-06 02:39:11 +0800

    added route for fetching gethub repository'

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request')
+const config = require('config')
 const auth = require('../../middleware/auth')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
@@ -237,5 +239,120 @@ router.delete('/experience/:exp_id', auth, async(req,res) => {
     }
 })
 
+//@route    PUT api/profile/education
+//@desc     Add profile education
+//@access   Private (Hence the middleware auth will be used)
+
+router.put('/education', [ 
+    auth, 
+    [
+        check('school', 'School is required')
+            .not()
+            .isEmpty(),
+        check('degree', 'Degree is required')
+            .not()
+            .isEmpty(),
+        check('fieldofstudy', 'Field of study date is required')
+            .not()
+            .isEmpty(),
+        check('from', 'From date is required')
+            .not()
+            .isEmpty()
+        
+    ]
+], 
+    async (req, res) => {
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({errors:errors.array()});
+        }
+
+        const {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        } = req.body;
+
+        const newEdu = {
+            school,
+            degree,
+            fieldofstudy,
+            from,
+            to,
+            current,
+            description
+        }
+
+        try {
+            const profile = await Profile.findOne({ user : req.user.id }); //Remember this is gotten from the auth (Otherwise you will have to signin for every single feauture once you do login )
+
+            await profile.education.unshift(newEdu); //The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
+            
+            await profile.save();
+
+            res.json(profile);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+            
+        }
+    }
+
+);
+
+//@route    DELETE api/profile/education/:edu_id
+//@desc     Remove profile education by the education ID
+//@access   Private (Hence the middleware auth will be used)
+
+router.delete('/education/:edu_id', auth, async (req,res) => {
+    try {
+        
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        //Get remove index
+        const removeIndex = profile.education
+            .map(item => item.id) //A new array with each element being the result of the callback function. So here it gives an array of item IDS
+            .indexOf(req.params.edu_id);
+        
+        profile.education.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error');  
+    }
+})
+
+//@route    GET api/profile/education/:edu_id
+//@desc     Get user repos from Github by username
+//@access   Public 
+
+router.get('/gethub/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method : 'GET',
+            headers: {'user-agent' : 'node.js'}
+        };
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+            if(response.statusCode != 200) {
+                res.status(404).json({msg: 'No Github profile found'});
+            }
+            res.json(JSON.parse(body));
+        });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
 
 module.exports = router;
