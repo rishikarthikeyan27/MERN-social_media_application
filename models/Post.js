35a11c05452207b419ad5729ac97029386bd3358
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-03-06 03:07:49 +0800

    Prepared the mongoose Schema for post routes

---
+++ models/Post.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text:{
+        type:String,
+        required: true
+    },
+    name:{
+        type:String
+    },
+    avatar: {
+        type:String
+    },
+    likes:[ //Because there will be an array of users who will like a post
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            }
+        }
+    ],
+    comments:[
+        {
+            user:{
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text:{
+                type:String,
+                required:true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date:{
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = Post = mongoose.model('post', PostSchema)
