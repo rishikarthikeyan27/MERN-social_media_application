f485fc513bc2ae39ce00d5d1f14b973d3d27979a
rishikarthikeyan27 <rishikar001@e.ntu.edu.sg>
2021-03-06 15:52:02 +0800

    Added routes to comment and remove comment

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -183,4 +183,82 @@ router.put('/unlike/:id', auth, async (req, res) => {
     }
 })
 
+//@route   PUT api/posts/comment/:id
+//@desc    Add a comment to the post
+//@access  Private (auth middleware)
+
+router.put('/comment/:id', [ 
+    auth,  
+    [
+        check('text', 'Text is required')
+            .not()
+            .isEmpty()
+    ]
+],
+async (req, res)=> {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array() })
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id); //So that we can get the post
+        const newComment = {
+            text: req.body.text,
+            name : user.name,
+            avatar : user.avatar,
+            user: req.user.id
+        };
+        post.comments.unshift(newComment);
+        await post.save();
+
+        res.json(post.comments);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');   
+    }
+})
+
+//@route   DELETE api/posts/comment/:postid/:comment_id
+//@desc    Delete comment by commentID
+//@access  Private (auth middleware)
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res)=> {
+    
+    try {
+        const post = await Post.findById(req.params.id); //So that we can get the post
+
+        //Pull out comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+        console.log(comment)
+
+        //Make sure comment exists
+        if(!comment){
+            return res.status(404).json({msg : 'Comment does not exist'});
+        }
+
+        //Make sure user deleting comment is the one who made the comment
+        //Check user
+        if(comment.user.toString() != req.user.id){
+            return res.status(401).json({msg : 'User not authorized'})
+        }
+
+        //Get removeIndex
+        const removeIndex = post.comments
+            .map(like => comment.user.toString()) //A new array with each element being the result of the callback function. So here it gives an array of item IDS
+            .indexOf(req.user.id);
+        console.log(post.comments.map(comment=> comment.user.toString()))
+        post.comments.splice(removeIndex,1)
+        await post.save();
+        res.json(post.comments);
+        
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');   
+    }
+})
+
+
 module.exports = router;
